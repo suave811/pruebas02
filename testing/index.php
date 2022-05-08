4fbfb5cab40c5f2d6cae488c14073091c3c4ad8b
Polarpass01 <victor.andrade03@inacapmail.cl>
2022-05-08 02:20:12 -0400

    Pruebas clase

--- testing/index.php
+++ testing/index.php
@@ -1,12 +1,50 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Indice de prueba 01</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+
+/*-------    Clase simple    -------*/
+  /*
+    class Persona {
+        public string $nombre = "Pepo";
+        public int $edad = 10;
+
+        public function mostrarVar(){
+            echo "Mi nombre es $this->nombre y tengo $this->edad años.";
+        }
+    }
+
+    $per = new Persona();
+    $per->mostrarVar();
+
+/*-------    Clase simple    -------*/
+
+/*----------------------------------*/
+
+/*------    Clase herencia    ------*/
+  /*
+    //final class Persona {   //Final no es posible heredar
+    class Persona {
+        public string $nombre = "Pepo";
+        public int $edad = 10;
+
+        public function mostrarVar(){
+            echo "Mi nombre es $this->nombre y tengo $this->edad años.";
+        }
+    }
+
+    class Hijo extends Persona {
+        public $apellido = 'Andrade';
+
+        public function mostrarVar(){
+            $this->edad = 20;
+
+            echo "Mi nombre es $this->nombre $this->apellido y tengo $this->edad años.<br />";
+            //echo __CLASS__;      //Ver clase
+        }
+    }
+
+    $ph = new Hijo();
+    $ph->mostrarVar();
+
+/*------    Clase herencia    ------*/
+
+
+?>
