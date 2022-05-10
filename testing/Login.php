d9ce12521bd2142c8efa3738691387891e663573
suave811 <claudio.flores@inacapmail.cl>
2022-05-10 13:35:03 -0400

    avances

---
+++ testing/Login.php
@@ -0,0 +1,47 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<LINK REL=StyleSheet HREF="visual.css" TYPE="text/css" MEDIA=screen>
+<script type="text/javascript" src="a.js"></script>
+
+<style>
+body {
+  background-image: url("https://st3.depositphotos.com/1017986/12848/i/950/depositphotos_128485498-stock-photo-school-classroom-with-desks.jpg");
+}
+
+
+</style>
+
+
+
+<div class="login-reg-panel">
+		<div class="login-info-box">
+			<h2>Olvidate la cuenta?</h2>
+			<p>Lorem ipsum dolor sit amet</p>
+			<label id="label-register" for="log-reg-show">Login</label>
+			<input type="radio" name="active-log-panel" id="log-reg-show"  checked="checked">
+		</div>
+							
+		<div class="register-info-box">
+			<h2>No teienes cuanta?</h2>
+			<p>Registrate</p>
+			<a href= "http://localhost/pruebas/pruebas02/testing/INDEX.php" id="label-login" for="log-login-show">Registrar</a>
+			<input type="radio" name="active-log-panel" id="log-login-show">
+		</div>
+							
+		<div class="white-panel">
+			<div class="login-show">
+				<h2>Entrar</h2>
+				<input type="text" placeholder="Email">
+				<input type="password" placeholder="Password">
+				<input type="button" value="Login">
+				<a href="">Olvide la contraseña</a>
+			</div>
+			<div class="register-show">
+				<h2>Registrar</h2>
+				
+			</div>
+		</div>
+	</div>
--- testing/index.php
+++ testing/index.php
@@ -1,50 +1,143 @@
-<?php
-
-/*-------    Clase simple    -------*/
-  /*
-    class Persona {
-        public string $nombre = "Pepo";
-        public int $edad = 10;
-
-        public function mostrarVar(){
-            echo "Mi nombre es $this->nombre y tengo $this->edad años.";
-        }
-    }
-
-    $per = new Persona();
-    $per->mostrarVar();
-
-/*-------    Clase simple    -------*/
-
-/*----------------------------------*/
-
-/*------    Clase herencia    ------*/
-  /*
-    //final class Persona {   //Final no es posible heredar
-    class Persona {
-        public string $nombre = "Pepo";
-        public int $edad = 10;
-
-        public function mostrarVar(){
-            echo "Mi nombre es $this->nombre y tengo $this->edad años.";
-        }
-    }
-
-    class Hijo extends Persona {
-        public $apellido = 'Andrade';
-
-        public function mostrarVar(){
-            $this->edad = 20;
-
-            echo "Mi nombre es $this->nombre $this->apellido y tengo $this->edad años.<br />";
-            //echo __CLASS__;      //Ver clase
-        }
-    }
-
-    $ph = new Hijo();
-    $ph->mostrarVar();
-
-/*------    Clase herencia    ------*/
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  <LINK REL=StyleSheet HREF="visual.css" TYPE="text/css" MEDIA=screen>
+<script type="text/javascript" src="a.js"></script>
+  <style type="text/css">
 
 
-?>
+	#regiration_form fieldset:not(:first-of-type) {
+		display: none;
+	}
+
+
+body {
+  background-image: url("https://st3.depositphotos.com/1017986/12848/i/950/depositphotos_128485498-stock-photo-school-classroom-with-desks.jpg");
+}
+
+
+
+
+  </style>
+
+  
+</head>
+<body>
+<div class="container">
+<h1>Registro de Apoderado</h1>
+<div class="progress">
+  <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
+
+<form id="regiration_form" novalidate action="Login.php"  method="post">
+
+<fieldset>
+  <h2>Paso 1: Crear su cuenta</h2>
+  <div class="form-group">
+  <label for="email">Email</label>
+  <input type="email" class="form-control" id="email" name="data[email]" placeholder="Email">
+</div>
+<div class="form-group">
+  <label for="exampleInputPassword1">Password</label>
+  <input type="password" class="form-control" id="password" placeholder="Password">
+</div>
+<div class="form-group">
+  <label for="exampleInputPassword1">Repetir Password</label>
+  <input type="password" class="form-control"  placeholder="Password">
+</div>
+
+  <input type="button" name="data[password]" class="next btn btn-info" value="Siguiente" />
+</fieldset>
+<fieldset>
+  <h2> Paso 2: Agregar detalles personales</h2>
+  <div class="form-group">
+  <label for="fName">Nombres</label>
+  <input type="text" class="form-control" name="data[fName]" id="fName" placeholder="Nombres">
+</div>
+<div class="form-group">
+  <label for="lName">Apellidos</label>
+  <input type="text" class="form-control" name="data[lName]" id="lName" placeholder="Apellidos">
+</div>
+<div class="form-group">
+  <label for="Arut">RUT</label>
+  <input type="text" class="form-control" name="data[Arut]" id="Arut" placeholder="Rut de apoderado">
+</div>
+<div class="form-group">
+  <label for="Acorreo">Correo</label>
+  <input type="text" class="form-control" name="data[Acorreo]" id="Acorreo" placeholder="Correo">
+</div>
+
+
+<div class="form-group">
+  <label for="Edad">Fecha de nacimiento</label>
+  <input type="date" class="form-control" name="data[Fedad]" id="Fedad" placeholder="Fecha de nacimiento">
+</div>
+
+  <input type="button" name="previous" class="previous btn btn-default" value="Previo" />
+  <input type="button" name="next" class="next btn btn-info" value="Siguiente" />
+
+
+
+</fieldset>
+<fieldset>
+  
+  <h2>Paso 3: Información de contacto</h2>
+  <div class="form-group">
+  <label for="mob">Numero Celular</label >
+  <input type="number" class="form-control" id="mob" name="data[mob]" placeholder="Numero Celular">
+</div>
+<div class="form-group">
+  <label for="address">Direccion</label>
+  <textarea  class="form-control" name="data[address]" placeholder="Direccion"></textarea>
+</div>
+<div class="form-group">
+  <label for="mob">Comuna</label >
+  <input type="text" class="form-control" id="mob" name="data[comu]" placeholder="Comuna">
+  </div>
+
+
+  <input type="button" name="previous" class="previous btn btn-default" value="Previo" />
+  <input type="submit" name="submit" class="submit btn btn-success" value="Enviar" id="submit_data" />
+
+  
+
+
+
+</fieldset>
+</form>
+</div>
+</body>
+</html>
+<script type="text/javascript">
+$(document).ready(function(){
+	var current = 1,current_step,next_step,steps;
+	steps = $("fieldset").length;
+	$(".next").click(function(){
+		current_step = $(this).parent();
+		next_step = $(this).parent().next();
+		next_step.show();
+		current_step.hide();
+		setProgressBar(++current);
+	});
+	$(".previous").click(function(){
+		current_step = $(this).parent();
+		next_step = $(this).parent().prev();
+		next_step.show();
+		current_step.hide();
+		setProgressBar(--current);
+	});
+	setProgressBar(current);
+	// Change progress bar action
+	function setProgressBar(curStep){
+		var percent = parseFloat(100 / steps) * curStep;
+		percent = percent.toFixed();
+		$(".progress-bar")
+			.css("width",percent+"%")
+			.html(percent+"%");		
+	}
+});
+</script>
